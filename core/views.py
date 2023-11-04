52c7349b21892e599579f1414174dd74a7ebca5f
chrishub132 <c.kratos@gmx.de>
2023-11-05 00:17:59 +0100

    worked on front-page

--- core/views.py
+++ core/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+from items.models import Category, Product
+
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    products = Product.objects.all
+    return render(request, 'core/index.html', {
+        'products': products,
+    })
 
 def list(request):
     return render(request, 'core/list.html')
